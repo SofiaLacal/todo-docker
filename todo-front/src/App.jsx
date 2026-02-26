38a51f1a8e85966251925a1683cf741fff7435a1
Sofia Lacal <sofialacrodriguez@outlook.com>
2026-02-26 12:38:04 +0100

    Estilo página principal

--- todo-front/src/App.jsx
+++ todo-front/src/App.jsx
@@ -1,21 +1,25 @@
 import './App.css'
+import imagen from './assets/img/mente-creativa.png'
 
 function App() {
 
   return (
     <>
-      <div>
+      <div className='contenedor-saludo'>
+        <h1>Bienvenid@ a To-Do</h1>
+        <img src={imagen} alt='Logo app' className='imagenLogo'/>
       </div>
-      <h1>Bienvenido a la lista de tareas</h1>
+
       <h2>Autores: Pablo Herrero, Javier Monzón, Sofía Lacal</h2>
+      
       <div className="contenedor-principal">
         <p>
           Entra al enlace para acceder a las diferentes listas
         </p>
         <div className='contenedor-enlaces'>
-          <p className='enlace'>Enlace 1</p>
-          <p className='enlace'>Enlace 2</p>
-          <p className='enlace'>Enlace 3</p>
+          <a className='enlace'href="">Compra</a>
+          <a className='enlace' href="">Tareas</a>
+          <a className='enlace' href="">Citas</a>
         </div>
       </div>
     </>
